d6a6bde6add8f2ea0397cc15addb619523abff14
Novita <nvita6513@gmail.com>
2022-10-26 16:34:04 +0700

    update fe

---
+++ src/views/Stock.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        Stock
+    </div>
+</template>
+
+<script>
+
+import API from '../utils/api'
+
+export default {
+    name: "Stock",
+    components: {
+
+    },
+    setup() {
+        // Create data / vars
+
+        // Get data
+
+        // Run data function
+
+        return {};
+    },
+    data() {
+        return {
+            stocks: []
+        }
+    },
+
+    methods: {
+        async getstocks() {
+            const result = await API.get("/stocks")
+
+            console.log(result)
+
+            this.stocks = result
+        },
+    },
+    mounted() {
+        this.getstocks()
+    },
+
+};
+</script>
---
+++ src/views/Update.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        Update Product
+    </div>
+</template>
+
+<script>
+
+import API from '../utils/api'
+
+export default {
+    name: "Product",
+    components: {
+
+    },
+    setup() {
+        // Create data / vars
+
+        // Get data
+
+        // Run data function
+
+        return {};
+    },
+    data() {
+        return {
+            products: []
+        }
+    },
+
+    methods: {
+        async updateproduct() {
+            const result = await API.get("/products")
+
+            console.log(result)
+
+            this.products = result
+        },
+    },
+    mounted() {
+        this.updateproduct()
+    },
+
+};
+</script>
